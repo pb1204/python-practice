5206977eb77de26ccf30ef7013276999083f6a43
pb1204 <peliah.l.bowers@wmich.edu>
2026-06-10 07:33:01 -0500

    Add files via upload

---
+++ # Learning Python.py
@@ -0,0 +1,23 @@
+# Learning Python
+
+print('Hi')
+
+# This is to learn the print function
+print("Hello World!")
+
+# This is for learning patterns with print function
+print("       1")
+print("     2 3 ")
+print("   4 5 6")
+print("7 8 9 10")
+
+# Learning the print function and comment
+#I love to read
+print('PPPPPPPP P   BBBBBBBBBBB')
+print('P        P   B         BBB')
+print('P        P   B         BBB')
+print('PPPPPPPP P   BBBBBBBBBBB') 
+print('P            B         BBB')
+print('P            B         BBB')
+print('P            BBBBBBBBBBB')
+
